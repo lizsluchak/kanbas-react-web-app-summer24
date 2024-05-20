826e0796eb6e26fbaf48943da8f8b7b28f8fe02d
sluchak <sluchak.e@northeastern.edu>
2024-05-20 17:01:47 -0400

    feat: a1.2.7 Paragraph Tags Added

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -11,8 +11,6 @@
 
 
 
-
-
 export default function Lab1() {
     return(
         <div id="wd-lab1">
@@ -32,7 +30,27 @@ export default function Lab1() {
                 h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest 
                 heading.
             </div>
-        </div>
+
+            <div id="wd-p-tag">
+                <h4>Paragraph Tag</h4>
+                <p id="wd-p-1"> ... </p>
+                <p id="wd-p-2">
+This is the first paragraph. The paragraph tag is used to format
+vertical gaps between long pieces of text like this one.
+        </p>
+        <p id="wd-p-3">
+This is the second paragraph. Even though there is a deliberate white
+gap between the paragraph above and this paragraph, by default
+browsers render them as one contiguous piece of text as shown here on
+the right.
+        </p>
+        <p id="wd-p-4">
+This is the third paragraph. Wrap each paragraph with the paragraph
+tag to tell browsers to render the gaps.
+        </p>
+
+            </div>
+            </div>
     );
 }
 
