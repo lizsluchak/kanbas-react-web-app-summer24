c8e0312bd0eb98adb3c4e5d9d5dd87889f972f4f
sluchak <sluchak.e@northeastern.edu>
2024-05-20 14:25:04 -0400

    feat: Exercise 2.6 - Heading Tags

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
+//c style language intergrated with html
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(
+  //the line below grabs the root id in public folder;
+  //index.html
+  //document is a global object that references document/object model
+  //document object provides low level apis that allow you to 
+  //add/remove/update content and inject it into root
   document.getElementById('root') as HTMLElement
 );
+
+  //once the root has been collected... render "App"
+  //what is "App"?, App is a function imported from file:
+  //"App.tsx", the file ending is optional.. go inside
+  //file "App.tsx" to see more about the App function grabbed
+  //App Function returns what we see in browser, function
+  //executes, spits out html, and gets combined with other
+  //content and content gets rendered into "root", root = index.html
 root.render(
   <React.StrictMode>
     <App />
