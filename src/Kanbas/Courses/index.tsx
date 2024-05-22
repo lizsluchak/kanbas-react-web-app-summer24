2c0b221cf10e0d4918799a2528a2bd127f0d3e2c
sluchak <sluchak.e@northeastern.edu>
2024-05-22 13:23:06 -0400

    a1.3.3.1 course navigation

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,5 +1,4 @@
-// import CoursesNavigation from "../Navigation";
-import KanbasNavigation from "../Navigation";
+import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import { Navigate, Route, Routes } from "react-router";
 
@@ -7,33 +6,31 @@ import { Navigate, Route, Routes } from "react-router";
 export default function Courses() {
     return (
 
-        <div id="wd-courses">
-      <h2>Course 1234</h2>
-    </div>
 
-    //   <div id="wd-courses">
-    //     <h2>Course 1234</h2>
-    //     <hr />
-    //   <table>
-    //     <tr>
-    //       <td valign="top">
-    //         <CoursesNavigation />
-    //       </td>
-    //       <td valign="top">
-    //         <Routes>
-    //           <Route path="/" element={<Navigate to="Home" />} />
-    //           <Route path="Home" element={<h3>Home</h3>} />
-    //     <Route path="Modules" element={<Modules />} />
-    //     <Route path="Assignments"
-    //            element={<h3>Assignments</h3>} />
-    //     <Route path="Assignments/:id"
-    //            element={<h3>Assignment Editor</h3>} />
 
-    //         </Routes>
-    //       </td>
-    //     </tr>
-    //   </table>
-    //   </div>
+      <div id="wd-courses">
+        <h2>Course 1234</h2>
+        <hr />
+      <table>
+        <tr>
+          <td valign="top">
+            <CoursesNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="Home" />} />
+              <Route path="Home" element={<h3>Home</h3>} />
+        <Route path="Modules" element={<Modules />} />
+        <Route path="Assignments"
+               element={<h3>Assignments</h3>} />
+        <Route path="Assignments/:id"
+               element={<h3>Assignment Editor</h3>} />
+
+            </Routes>
+          </td>
+        </tr>
+      </table>
+      </div>
   );}
 
   //i had to add this export line.. why?
