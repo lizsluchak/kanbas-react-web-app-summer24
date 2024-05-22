b5c19e9276070057c42311c5289d70ea0feceb28
sluchak <sluchak.e@northeastern.edu>
2024-05-22 14:32:19 -0400

    a1.3.6 Assignments Screen

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,6 +1,7 @@
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 import { Navigate, Route, Routes } from "react-router";
 
 
@@ -24,8 +25,7 @@ export default function Courses() {
               <Route path="Home" element={<Home />} />
 
         <Route path="Modules" element={<Modules />} />
-        <Route path="Assignments"
-               element={<h3>Assignments</h3>} />
+        <Route path="Assignments" element={<Assignments />} />
         <Route path="Assignments/:id"
                element={<h3>Assignment Editor</h3>} />
 
