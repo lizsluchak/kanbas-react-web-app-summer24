1b2d1f48d0fe35eb01f5c4be92b3a741a5d9a999
sluchak <sluchak.e@northeastern.edu>
2024-05-22 13:36:24 -0400

    feat: a1.3.4 - Implementing the Modules Scren

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -16,8 +16,10 @@ export default function Modules() {
 
         <ul id="wd-modules">
           <li className="wd-module">
-            <div className="wd-title">Week 1</div>
-            <ul className="wd-lessons">
+
+            <div className="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div>
+              <ul className="wd-lessons">
+              
               <li className="wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
                 <ul className="wd-content">
@@ -25,13 +27,46 @@ export default function Modules() {
                   <li className="wd-content-item">Learn what is Web Development</li>
                 </ul>
               </li>
+
+              <li className="wd-lesson">
+                <span className="wd-title">READING</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Full Stack Developer - Chapter 1</li>
+                  <li className="wd-content-item">Full Stack Developer - Chapter 2</li>
+                </ul>
+              </li>
+
+              <li className="wd-lesson">
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to Web Development</li>
+                  <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+                  <li className="wd-content-item">Creating a React Application</li>
+                </ul>
+              </li>
+
+
+
             </ul>
           </li>
           <li className="wd-module">
             <div className="wd-title">Week 2</div>
             <ul className="wd-lessons">
               <li className="wd-lesson">
+
                 <span className="wd-title">LEARNING OBJECTIVES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
+                  <li className="wd-content-item">Deploy the assignment to Netlify</li>
+                </ul>
+
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to HTML and the DOM</li>
+                  <li className="wd-content-item">Formatting Web content with Headings</li>
+                  <li className="wd-content-item">Formatting content with Lists and Tables</li>
+                </ul>
+
               </li>
             </ul>
           </li>
