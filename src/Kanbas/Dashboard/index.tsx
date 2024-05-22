42ae6d27cbe148b4e922a3b0b7d870ccec2d7161
sluchak <sluchak.e@northeastern.edu>
2024-05-22 10:06:16 -0400

    feat: 7 dummy courses added to dashboard

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,8 +1,12 @@
 export default function Dashboard() {
     return (
       <div id="wd-dashboard">
+
+
         <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
         <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+
+{/* course 1: */}
         <div id="wd-dashboard-courses">
           <div className="wd-dashboard-course">
             <img src="/images/reactjs.jpg" width={200} />
@@ -17,9 +21,114 @@ export default function Dashboard() {
               <a href="#/Kanbas/Courses/1234/Home"> Go </a>
             </div>
           </div>
-          <div className="wd-dashboard-course"> ... </div>
-          <div className="wd-dashboard-course"> ... </div>
+
+
+        {/* course 2: */}
+          <div className="wd-dashboard-course"> 
+          <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1234/Home">
+                CS1234 React JS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+          </div>
+
+
+
+
+
+           {/* course 3: */}
+           <div className="wd-dashboard-course"> 
+          <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1234/Home">
+                CS1234 React JS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+          </div>
+
+
+
+           {/* course 4: */}
+           <div className="wd-dashboard-course"> 
+          <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1234/Home">
+                CS1234 React JS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+            </div>
+
+
+             {/* course 5: */}
+          <div className="wd-dashboard-course"> 
+          <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1234/Home">
+                CS1234 React JS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+          </div>
+          
+
+      {/* course 6: */}
+      <div className="wd-dashboard-course"> 
+          <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1234/Home">
+                CS1234 React JS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+          </div>
+
+
+                {/* course 7: */}
+                <div className="wd-dashboard-course"> 
+          <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1234/Home">
+                CS1234 React JS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+          </div>
+
+
+
+
+
         </div>
+
+
       </div>
   );}
   
