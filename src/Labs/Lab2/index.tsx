d876add3999ff2f900a0d4c7ca11e9dc28bd47b1
sluchak <sluchak.e@northeastern.edu>
2024-05-26 15:11:50 -0400

    feat: 3.1.1 Styling Elements with the Style Attribute

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,9 +1,31 @@
+
+
 export default function Lab2() {
     return(
         <div>
-            <h2>Lab 2</h2>
+
+            <div>
+            <h2>Lab 2 - Cascading Style Sheets</h2>
+            <h4><b>Learning Objectives:</b></h4>
+            <ol id="wd-lab2-objectives">
+                <li>Style web content with Cascading Style Sheets</li>
+                <li>Layout and responsive design with Bootstrap</li>
+                <li>Use react icons</li>
+            </ol>
+            </div>
+            
+
+            <div>
+            <h3>Styling with the STYLE attribute</h3>
+            <p style={{ backgroundColor: "blue",
+                        color: "white" }}>
+            Style attribute allows configuring look and feel
+            right on the element. Although it's very convenient
+            it is considered bad practice and you should avoid
+            using the style attribute
+            </p>
+            </div>
+            
         </div>
     );
 }
-
-export {}
