cff07b6565902b12432a0cffebe3ab7fec0d54bc
sluchak <sluchak.e@northeastern.edu>
2024-05-29 15:36:33 -0400

    feat: 3.1.5 selecting content based on the document structure

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -67,6 +67,36 @@ export default function Lab2() {
             This heading has same style as paragraph above. </h4>
         </div>
 
+        
+        <div id="wd-css-document-structure">
+            {/*wd-selector-1 is a parent element*/}
+            <div className="wd-selector-1">
+            <h3>Document structure selectors</h3>
+                
+                {/*wd-selector-2 is a direct child of wd-selector-1*/}
+                <div className="wd-selector-2">
+                Selectors can be combined to refer elements in particular
+                places in the document.
+
+                {/*wd-selector-3 is a direct child of wd-selector-2 and a
+                descendent of wd-selector-1*/}
+                <p className="wd-selector-3">
+                    This paragraph's red background is referenced as
+                    <br />
+                    .selector-2 .selector3<br />
+                    meaning the descendant of some ancestor.<br />
+                    
+                    {/*wd-selector-4 is a direct child of wd-selector-3 and a
+                    descendent of wd-selector-1 and wd-selector-2*/}
+                    <span className="wd-selector-4">
+                    Whereas this span is a direct child of its parent
+                    </span><br />
+                    You can combine these relationships to create specific 
+                    styles depending on the document structure
+                </p>
+                </div>
+            </div>
+        </div>
 
 
         </div>
