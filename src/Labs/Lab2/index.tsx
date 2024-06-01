59c0b6df97d78ea45b6ef4d1a773292c7e19aa71
sluchak <sluchak.e@northeastern.edu>
2024-06-01 12:49:20 -0400

    feat: 3.1.19 Flex

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -526,6 +526,56 @@ export default function Lab2() {
 </div>
 
 
+{/*--------------------------------------------------------------------------*/
+/*--------------------------------3.1.19 Flex--------------------------------*/
+/*---------------------------------------------------------------------------*/}
+
+<div id="wd-css-flex">
+    <h2>Flex</h2>
+    
+    {/*Flex Elements without Growth*/}
+    <div className="wd-flex-row-container">
+        <div className="wd-bg-color-yellow">Column 1</div>
+            <div className="wd-bg-color-blue">Column 2</div>
+                <div className="wd-bg-color-red">Column 3</div>
+    </div>
+
+
+    {/*Flex Elements with Growth/Expansion*/} 
+    <div className="wd-flex-row-container">
+        <div className="wd-bg-color-yellow">Column 1</div>
+            <div className="wd-bg-color-blue">Column 2</div>
+                <div className="wd-bg-color-red
+                    wd-flex-grow-1">
+                    Column 3
+                </div>
+    </div>
+    
+
+    <div className="wd-flex-row-container">
+        <div className="wd-bg-color-yellow 
+            wd-width-75px">
+            Column 1
+        </div>
+        
+        <div className="wd-bg-color-blue">Column 2</div>
+        <div className="wd-bg-color-red
+            wd-flex-grow-1">
+            Column 3
+        </div>
+    </div>
+
+
+
+
+
+</div>
+
+
+
+
+
+
 
 
 
@@ -561,5 +611,3 @@ export default function Lab2() {
     </div>
     );
 }
-
-
