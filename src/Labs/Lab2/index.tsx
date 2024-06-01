38d4e58838c568e9d2e4ee49fe7c61d8fa41b80d
sluchak <sluchak.e@northeastern.edu>
2024-06-01 13:41:41 -0400

    feat: 3.3.3 laying out content with grids

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 export default function Lab2() {
     return(
         //main lab2 div
+        //font changed by adding container but not margins
         <div id = "container-fluid">
             
             <div id = "wd-lab2-learningObjectives">
@@ -617,9 +618,49 @@ export default function Lab2() {
         class just defines a constant thin margin all around the document.
 */}
 
+{/*--------------------3.3.3 Laying Out Content with Grids-------------------*/}
 
+<div>
+    <h2>Bootstrap</h2>
+    <div id="wd-bs-grid-system">
+    
+        <h2>Grid system</h2>
 
+        <div className="row">
+            <div className="col bg-danger text-white">
+                <h3>Left half</h3>
+            </div>
+            
+            <div className="col bg-primary text-white">
+                <h3>Right half</h3>
+            </div>
+        </div>
 
+        <div className="row">
+            <div className="col-4 bg-warning">
+                <h3>One thirds</h3>
+            </div>
+            
+            <div className="col-8 bg-success text-white">
+                <h3>Two thirds</h3>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-2 bg-dark text-white">
+                <h3>Sidebar</h3>
+            </div>
+            
+            <div className="col-8 bg-secondary text-white">
+                <h3>Main content</h3>
+            </div>
+            
+            <div className="col-2 bg-info">
+                <h3>Sidebar</h3>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
