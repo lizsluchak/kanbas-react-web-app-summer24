0dda577d87ae17e9e197a1ad56cef1ec1686b9f6
sluchak <sluchak.e@northeastern.edu>
2024-06-01 12:15:35 -0400

    feat 3.1.16 Z Index

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -386,8 +386,51 @@ export default function Lab2() {
 </div>
 
 
+{/*--------------------------------------------------------------------------*/
+/*----------------------------3.1.16 Styling Z Index-------------------------*/
+/*---------------------------------------------------------------------------*/}
+
+<div id="wd-z-index">
+    <h2>Z index</h2>
+
+    <div className="wd-pos-relative">
+        <div className="wd-pos-absolute-10-10 
+            wd-bg-color-yellow 
+            wd-dimension-portrait">
+            Portrait
+        </div>
+    
+        <div className="wd-zindex-bring-to-front 
+            wd-pos-absolute-50-50 
+            wd-dimension-landscape
+            wd-bg-color-blue 
+            wd-fg-color-white">
+            Landscape
+        </div>
+
+        <div className="wd-pos-absolute-120-20 
+            wd-bg-color-red 
+            wd-dimension-square">
+            Square
+        </div>
+    </div>
+    
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+</div>
+
+
+
+
 
 {/* main div ending----------------------------------------------------------*/}
     </div>
     );
 }
+
+
