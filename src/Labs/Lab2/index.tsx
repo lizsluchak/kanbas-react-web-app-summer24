c4fc73cc572beb9d84ce38c2406cbaeb66b612fb
sluchak <sluchak.e@northeastern.edu>
2024-06-01 12:09:24 -0400

    feat 3.1.15 Styling Fixed Position

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -363,9 +363,27 @@ export default function Lab2() {
     <br />
     <br />
     <br />
-    
+
 </div>
 
+
+{/*--------------------------------------------------------------------------*/
+/*------------------------3.1.15 Styling Fixed Position----------------------*/
+/*---------------------------------------------------------------------------*/}
+<div id="wd-css-position-fixed">
+    <h2>Fixed position</h2>
+    
+    Checkout the blue square that says "Fixed position" stuck all the way on 
+    the right and half way down the page. It doesn't scroll with the rest of 
+    the page. Its position is "Fixed".
+    
+    <div className="wd-pos-fixed 
+        wd-dimension-square 
+        wd-bg-color-blue 
+        wd-fg-color-white">
+        Fixed position
+    </div>
+</div>
 
 
 
