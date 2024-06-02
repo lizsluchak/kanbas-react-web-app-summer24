f9f9be87c35d96a3f34e9617b6e77d43c6781937
sluchak <sluchak.e@northeastern.edu>
2024-06-02 12:28:48 -0400

    feat: 4.1 Implementing Responsive Design on Kanbas

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,17 +1,33 @@
+import ScreenSizeLabel from "../../../Labs/Lab2/ScreenSizeLabel";
 import Modules from "../Modules";
 import CourseStatus from "./Status";
 
 export default function Home() {
   return (
-    <table id="wd-home">
-      <tr>
-        <td valign="top">
-          <Modules />
-        </td>
-        <td valign="top">
-          <CourseStatus />
-        </td>
-      </tr>
-    </table>
+    
+    <div id="wd-home" className="d-flex">
+      <ScreenSizeLabel/>
+      
+      <div className="flex-fill me-5">
+        <Modules />
+      </div>
+
+      <div className="d-none d-xl-block">
+        <CourseStatus />
+      </div>
+
+    </div>
+
   );
 }
+
+
+
+/**
+ * Iteration Gravyard:
+ * 
+ *  (1) Version 1 discarded to use flex/div instead of
+ *      table elements
+ * 
+ * 
+ */
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,17 +1,39 @@
 import Dashboard from "./Dashboard";
-
 import KanbasNavigation from "./Navigation";
-
-
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
-
+import TOC2 from "../Labs/Lab2/PillNavigation";
 
 
 export default function Kanbas() {
     return (
+
       <div id="wd-kanbas">
-            <table>
+        <div className="d-flex">
+          
+          <div className="d-none d-md-block bg-black">
+            <KanbasNavigation />
+          </div>
+
+          <div className="flex-fill p-4">
+            <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Dashboard" element={<Dashboard />} />
+              <Route path="Courses/:cid/*" element={<Courses />} />
+            </Routes>
+          </div>
+      </div>
+    </div>
+
+);}
+
+
+
+
+{/* Previous Iterations:
+
+  (1) I dont remember why we stopped using this one:
+      <table>
         <tr>
           <td valign="top">
             <KanbasNavigation />
@@ -28,29 +50,39 @@ export default function Kanbas() {
           </td>
         </tr>
       </table>
+  (2) We stopped using the below version because it uses table elements
+      to layout screens horizontally ~ generally considered a bad practice.
+      Instead we switch to implementing it with DIVs and CSS flex instead
+      of tables:
+            <div id="wd-kanbas">
+            <table>
+              <tr>
+                <td valign="top">
+                  <KanbasNavigation />
+                </td>
 
+                <td valign="top">
+                  <Routes>
+                    <Route path="/" element={<Navigate to="Dashboard" />} />
+                    <Route path="Account" element={<h1>Account</h1>} />
+                    <Route path="Dashboard" element={<Dashboard />} />
+                    <Route path="Courses/:id/*" element={<Courses />} />
+                    <Route path="Calendar" element={<h1>Calendar</h1>} />
+                    <Route path="Inbox" element={<h1>Inbox</h1>} />
+                  </Routes>
+                </td>
+              </tr>
+      </table>
+    </div>
+    
+    
+    
+    
+    
+*/}
 
-
-        {/* <table>
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="Account" element={<h1>Account</h1>} />
-              <Route path="Dashboard" element={<Dashboard />} />
-              <Route path="Courses/:id/*" element={<Courses />} />
-              <Route path="Calendar" element={<h1>Calendar</h1>} />
-              <Route path="Inbox" element={<h1>Inbox</h1>} />
-            </Routes>
-          </td>
-        </tr>
-      </table> */}
-
-      </div>
-  );}
+      
+  
 
 //   //i had to add this export line.. why?
 // export {}
