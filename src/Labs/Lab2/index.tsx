4b6aed5ad35b78ca2381d835859a0fa3ce7cf5ca
sluchak <sluchak.e@northeastern.edu>
2024-06-02 10:35:25 -0400

    feat: 3.3.9 Styling Lists of Hyperlinks

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -904,9 +904,47 @@ export default function Lab2() {
 </div>
 
 
+{/*--------------------------------------------------------------------------*/
+/*--------------------3.3.9 Styling List of Hyperlinks-----------------------*/
+/*---------------------------------------------------------------------------*/
+/*
+    -   The same .list-group and .list-group-item Bootstrap classes can be 
+        applied to div and a tags to implement a list of anchor links. 
+    -   To practice with Bootstrap hyperlink lists, copy the HTML code below 
+        to the end of index.tsx, and save. 
+    -   Refresh the browser and confirm it looks similar to image shown and 
+        that the links work. 
+*/}
 
-
-
+<div id="wd-css-hyperlink-list">
+    <h3>Favorite books</h3>
+    <div className="list-group">
+        <a href="https://en.wikipedia.org/wiki/Dune_(novel)" 
+            className="list-group-item list-group-item-action active">
+            Dune
+        </a>
+        
+        <a href="https://en.wikipedia.org/wiki/The_Lord_of_the_Rings" 
+            className="list-group-item list-group-item-action">
+            Lord of the Rings
+        </a>
+        
+        <a href="https://en.wikipedia.org/wiki/The_Forever_War" 
+            className="list-group-item list-group-item-action">
+            The Forever War
+        </a>
+        
+        <a href="https://en.wikipedia.org/wiki/2001:_A_Space_Odyssey_(novel)" 
+            className="list-group-item list-group-item-action">
+            2001 A Space Odyssey
+        </a>
+        
+        <a href="https://en.wikipedia.org/wiki/Ender%27s_Game" 
+            className="list-group-item list-group-item-action disabled">
+            Ender's Game
+        </a>
+    </div>
+</div>
 
 
 
