fef648facefec983f638a5d68b43c83eeb5beae1
sluchak <sluchak.e@northeastern.edu>
2024-06-03 12:55:24 -0400

    feat: course nav bar styled

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,15 +1,34 @@
+import "./index.css";
+
 export default function CoursesNavigation() {
   return (
-    <ul id="wd-courses-navigation">
-      <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/1234/Home">Home</a></li>
-      <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/1234/Modules">Modules</a></li>
-      <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/1234/Piazza">Piazza</a></li>
-      <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/1234/Zoom">Zoom</a></li>
-      <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/1234/Assignments">Assignments</a></li>
-      <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/1234/Quizzes">Quizzes</a></li>
-      <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/1234/Grades">Grades</a></li>
-    </ul>
-);}
+    <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
+      
+        <a id="wd-course-home-link" href="#/Kanbas/Courses/1234/Home"
+          className="text-decoration-underline list-group-item active border border-0 link-offest-1"> Home </a>
+        
+        <a id="wd-course-modules-link" href="#/Kanbas/Courses/1234/Modules"
+          className="text-decoration-underline link-underline-danger link-offest-1 list-group-item link-underline-danger text-danger border border-0"> Modules </a>
+        
+        <a id="wd-course-piazza-link" href="#/Kanbas/Courses/1234/Piazza"
+          className="text-decoration-underline link-underline-danger link-offest-1 list-group-item link-underline-danger text-danger border border-0"> Piazza </a>
+        
+        <a id="wd-course-zoom-link" href="#/Kanbas/Courses/1234/Zoom"
+          className="text-decoration-underline link-underline-danger link-offest-1 list-group-item text-danger border border-0"> Zoom </a>
+        
+        <a id="wd-course-quizzes-link" href="#/Kanbas/Courses/1234/Assignments"
+          className="text-decoration-underline link-underline-danger link-offest-1 list-group-item text-danger border border-0"> Assignments </a>
+        
+        <a id="wd-course-assignments-link" href="#/Kanbas/Courses/1234/Quizzes"
+          className="text-decoration-underline link-underline-danger link-offest-1 list-group-item text-danger border border-0"> Quizzes </a>
+        
+        <a id="wd-course-grades-link" href="#/Kanbas/Courses/1234/Grades"
+          className="text-decoration-underline link-underline-danger link-offest-1 list-group-item text-danger border border-0"> Grades </a>
+    </div>
+
+  );
+}
+
 
 
 
