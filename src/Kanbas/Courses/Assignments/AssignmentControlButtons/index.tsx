96c515aa0e8010f05d798817139ceff4b0a1c906
sluchak <sluchak.e@northeastern.edu>
2024-06-05 22:42:36 -0400

    feat: Assignment screen

--- src/Kanbas/Courses/Assignments/AssignmentControlButtons/index.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentControlButtons/index.tsx
@@ -1,11 +1,10 @@
 import { IoEllipsisVertical } from "react-icons/io5";
-
 import { BsPlus } from "react-icons/bs";
 
 export default function AssignmentControlButtons() {
   return (
     <div className="float-end me-1">
-      <BsPlus style={{ fontSize: '30px' }}/> 
+      <BsPlus className="me-2" style={{ fontSize: '30px' }}/> 
       <IoEllipsisVertical className="fs-4" />
     </div>
 );}
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,131 +1,108 @@
 import AssignmentControlButtons from "./AssignmentControlButtons";
 import AssignmentControls from "./AssignmentControls";
 import { BsGripVertical } from "react-icons/bs";
-import ModuleControlButtons from "../Modules/ModuleControlButtons"
+import ModuleControlButtons from "../Modules/ModuleControlButtons";
 import LessonControlButtons from "../Modules/LessonControlButtons";
+import { FaCaretDown } from "react-icons/fa";
+import { FaFilePen } from "react-icons/fa6";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Assignments() {
-    return (
+  return (
+    <div id="wd-assignments">
+      <div id="wd-assignment-controls-bar">
+        <div>
+          <AssignmentControls />
+        </div>
+      </div>
 
-      <div id="wd-assignments">
-        
-        <div id="wd-assignment-controls-bar">
-          
-          <div>
-            <AssignmentControls />
+      <div id="wd-assignments-list" className="list-group rounded-0">
+        <li className="list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 list-group-item list-group-item-active" style={{ backgroundColor: "#ffe6e6", color: "#000" }}>
+            <button style={{ all: 'unset', cursor: 'pointer' }}> 
+              <BsGripVertical className="me-2 fs-3" />
+              <FaCaretDown />
+              <strong> ASSIGNMENTS </strong>
+            </button>
+            <ModuleControlButtons />
           </div>
 
-          
-          <div className="bg-info">
-
-            <ul id="wd-assignments" className="list-group rounded-0">
-    
-    
-              <li className="list-group-item p-0 mb-5 fs-5 border-gray">
-                <div className="wd-title p-3 ps-2 bg-red">
-                  <BsGripVertical className="me-2 fs-3" />
-                  Week 1
-                  <ModuleControlButtons />
-                </div>
-                <ul className="wd-lessons list-group rounded-0">
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    LEARNING OBJECTIVES
-                    <LessonControlButtons />
-                  </li>
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    Introduction to the course
-                    <LessonControlButtons />
-                  </li>
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    Learn what is Web Development
-                    <LessonControlButtons />
-                  </li>
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    LESSON 1
-                    <LessonControlButtons />
-                  </li>
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    LESSON 2
-                    <LessonControlButtons />
-                  </li>
-                </ul>
-              </li>
-
-              <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-                <div className="wd-title p-3 ps-2 bg-secondary">
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item list-group-item-active p-3 ps-1 d-flex flex-row">
+              <div className="d-flex flex-row justify-content-between align-items-center">
                 <BsGripVertical className="me-2 fs-3" />
-                  Week 2
-                  <ModuleControlButtons />
+                <FaFilePen className="text-success me-4 fs-3" />
+              </div>
+
+              <div className="ms-3 d-flex flex-column flex-fill">
+                <h4><strong>A1</strong></h4>
+                <div className="d-flex flex-row">
+                  <p className="m-1 text-danger">Multiple Modules</p>
+                  <p className="m-1">|</p>
+                  <p className="m-1"><strong>Not available until</strong></p>
+                  <p className="m-1">May 6 at 12:00am |</p>
                 </div>
-                <ul className="wd-lessons list-group rounded-0">
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    LEARNING OBJECTIVES
-                    <LessonControlButtons />
-                  </li>
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    LESSON 1
-                    <LessonControlButtons />
-                  </li>
-                  <li className="wd-lesson list-group-item p-3 ps-1">
-                  <BsGripVertical className="me-2 fs-3" />
-                    LESSON 2
-                    <LessonControlButtons />
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            
-            
-            <h3 id="wd-assignments-title">
-              ASSIGNMENTS 40% of Total <button>+</button>
-            </h3>
-
-            <ul id="wd-assignment-list">
-              <li className="wd-assignment-list-item">
-                <a className="wd-assignment-link"
-                  href="#/Kanbas/Courses/1234/Assignments/123">
-                  A1 - ENV + HTML
-                  </a>
-                  <div>Multiple Modules | <b>Not available until </b> May 6 at 12:00am |</div>
-                  <div><b>Due </b>May 13 at 11:59pm | 100 pts</div>
+                <div className="d-flex flex-row">
+                  <p className="m-1"><strong>Due</strong></p>
+                  <p className="m-1">May 13 at 11:59pm | 100 pts</p>
+                </div>
+              </div>
+              <div className="d-flex flex-row ms-auto align-items-center">
+                <LessonControlButtons />
+              </div>
             </li>
 
-            <li className="wd-assignment-list-item">
-              {/* Complete On Your Own */}
-              <a className="wd-assignment-link"
-                  href="#/Kanbas/Courses/1234/Assignments/123">
-                  A2 - CSS + BOOTSTRAP
-                  </a>
-                  <div>Multiple Modules | <b>Not available until </b> May 13 at 12:00am |</div>
-                  <div><b>Due </b>May 20 at 11:59pm | 100 pts</div>
+            <li className="wd-lesson list-group-item list-group-item-active p-3 ps-1 d-flex flex-row">
+              <div className="d-flex flex-row justify-content-between align-items-center">
+                <BsGripVertical className="me-2 fs-3" />
+                <FaFilePen className="text-success me-4 fs-3" />
+              </div>
+
+              <div className="ms-3 d-flex flex-column flex-fill">
+                <h4><strong>A2</strong></h4>
+                <div className="d-flex flex-row">
+                  <p className="m-1 text-danger">Multiple Modules</p>
+                  <p className="m-1">|</p>
+                  <p className="m-1"><strong>Not available until</strong></p>
+                  <p className="m-1">May 13 at 12:00am |</p>
+                </div>
+                <div className="d-flex flex-row">
+                  <p className="m-1"><strong>Due</strong></p>
+                  <p className="m-1">May 20 at 11:59pm | 100 pts</p>
+                </div>
+              </div>
+              <div className="d-flex flex-row ms-auto align-items-center">
+                <LessonControlButtons />
+              </div>
             </li>
 
-            <li className="wd-assignment-list-item">
-              {/* Complete On Your Own */}
-              <a className="wd-assignment-link"
-                  href="#/Kanbas/Courses/1234/Assignments/123">
-                  A3 - JAVASCRIPT + REACT
-                  </a>
-                  <div>Multiple Modules | <b>Not available until </b> May 20 at 12:00am |</div>
-                  <div><b>Due </b>May 27 at 11:59pm | 100 pts</div>
+            <li className="wd-lesson list-group-item list-group-item-active p-3 ps-1 d-flex flex-row">
+              <div className="d-flex flex-row justify-content-between align-items-center">
+                <BsGripVertical className="me-2 fs-3" />
+                <FaFilePen className="text-success me-4 fs-3" />
+              </div>
+
+              <div className="ms-3 d-flex flex-column flex-fill">
+                <h4><strong>A3</strong></h4>
+                <div className="d-flex flex-row">
+                  <p className="m-1 text-danger">Multiple Modules</p>
+                  <p className="m-1">|</p>
+                  <p className="m-1"><strong>Not available until</strong></p>
+                  <p className="m-1">May 20 at 12:00am |</p>
+                </div>
+                <div className="d-flex flex-row">
+                  <p className="m-1"><strong>Due</strong></p>
+                  <p className="m-1">May 27 at 11:59pm | 100 pts</p>
+                </div>
+              </div>
+              <div className="d-flex flex-row ms-auto align-items-center">
+                <LessonControlButtons />
+              </div>
             </li>
-            </ul>
-
-          </div>
-
-
-          </div>
-          </div>
-
-        
-
-);
-
+          </ul>
+        </li>
+      </div>
+    </div>
+  );
 }
