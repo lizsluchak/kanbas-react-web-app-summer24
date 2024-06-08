5f8e4f43c06e060602070d42c3d4f33b872a8471
sluchak <sluchak.e@northeastern.edu>
2024-06-08 13:01:38 -0400

    feat: assignment editor layout

--- src/Kanbas/Courses/Assignments/Editor/index.tsx
+++ src/Kanbas/Courses/Assignments/Editor/index.tsx
@@ -1,50 +1,152 @@
 export default function AssignmentEditor() {
     return (
-      <div id="outmost div">
-        <div id="flex container" className="bg-black d-flex">
-          <div className="flex-column flex-fill bg-info">
-            <div>
-              Input Label
+      <div id="divA-mother">
+        
+        
+        {/** divB1-full width column------------------------------------------- */}
+        <div id="divB1-flex-column" className="d-flex ms-auto bg-black">
+          <div className="flex-column flex-fill">
+          <div className="mb-3">
+              <label htmlFor="assignment1"
+                  className="row-sm-2 row-form-label p-1">
+                  <h5>Assignment Name</h5>
+              </label>
+
+              <div>
+                  <input type="text" 
+                      className="form-control"
+                      id="assignmentTitle1"
+                      placeholder="A1">
+                        
+                      </input>
+              </div>
+
+              <label htmlFor="assignmentDescription1"
+                  className="row-sm-2 row-form-label p-1">
+              </label>
+
+              <div>
+                  <textarea 
+                      className="form-control"
+                      id="assignmentTitle1" 
+                      rows={10}
+                      placeholder="The assignment is available online Submit a 
+                      link to the landing page of your Web application running on Netlify. the The landing page should include the following: Your full name and section links to each of the lab assignments, link to the Kanbas application, Links to all relevant source code respositories. The kanbas application should include a link to navigate back to the landing page.
+                      ">
+                      </textarea>
+              </div>
             </div>
 
-            <div>
-              Assigment name input box
-            </div>
+          
+            {/** divB2-row wiht 2 columns---------------------------------------- */}
+            <div className="bg-secondary">
+              <div className="bg-danger d-flex flex-row">
+                <div className="col-5">
+                    <label htmlFor="points"
+                        className="col-form-label float-end me-5">
+                        Points
+                    </label>
+                </div>
 
-            <div>
-              Assignment editor box
-            </div>
+                <div className="col-7">
+                <input type="text" 
+                            className="form-control"
+                            id="points" 
+                            value="100" />
+                </div>
 
-            <div className="d-flex flex-column align-items-end">
-              <div>
-                points
-              </div>
-              <div>
-                assignment group
-              </div>
-              <div>
-                assignment group
-              </div>
-              <div>
-                assignment group
-              </div>
-              <div>
-                assignment group
-              </div>
             </div>
             
+          
+            </div>
 
             <div className="bg-secondary">
               <div className="bg-danger d-flex flex-row">
                 <div className="col-5">
-                  column 1
+                    <label htmlFor="points"
+                        className="row-form-label float-end me-5">
+                        Assignment Group
+                    </label>
                 </div>
+      
+
                 <div className="col-7">
-                  column2
+                <input type="text" 
+                            className="form-control"
+                            id="points" 
+                            value="100" />
                 </div>
+
             </div>
-          
             </div>
+
+
+
+            <div className="bg-secondary">
+              <div className="bg-danger d-flex flex-row">
+                <div className="col-5">
+                    <label htmlFor="points"
+                        className="row-form-label float-end me-5">
+                        Display Grade as
+                    </label>
+                </div>
+      
+
+                <div className="col-7">
+                <input type="text" 
+                            className="form-control"
+                            id="points" 
+                            value="100" />
+                </div>
+
+            </div>
+            </div>
+
+
+            <div className="bg-secondary">
+              <div className="bg-danger d-flex flex-row">
+                <div className="col-5">
+                    <label htmlFor="points"
+                        className="row-form-label float-end me-5">
+                        Submission Type
+                    </label>
+                </div>
+      
+
+                <div className="col-7">
+                <input type="text" 
+                            className="form-control"
+                            id="points" 
+                            value="100" />
+                </div>
+
+            </div>
+            </div>
+
+
+
+
+            <div className="bg-secondary">
+              <div className="bg-danger d-flex flex-row">
+                <div className="col-5">
+                    <label htmlFor="points"
+                        className="row-form-label float-end me-5">
+                        Assign
+                    </label>
+                </div>
+      
+
+                <div className="col-7">
+                <input type="text" 
+                            className="form-control"
+                            id="points" 
+                            value="100" />
+                </div>
+
+            </div>
+            </div>
+
+
             </div>
 
     
