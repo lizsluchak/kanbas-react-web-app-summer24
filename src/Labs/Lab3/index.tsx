8948a655739727d2eb8e0b283a369d810a2704bc
sluchak <sluchak.e@northeastern.edu>
2024-06-15 17:11:34 -0400

    A3: 2.2 JS basics, set up lab3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,8 +1,8 @@
 export default function Lab3() {
     return(
-        <div>
-            <h2>Lab 3</h2>
-        </div>
+        <div id="wd-lab3" className="container-fluid">
+        <h3>Lab 3</h3>
+      </div>
     );
 }
 
