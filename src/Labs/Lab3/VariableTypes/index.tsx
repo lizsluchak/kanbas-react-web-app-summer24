3fb2ea8cd237b336164a34896a8003c4bcb68b51
sluchak <sluchak.e@northeastern.edu>
2024-06-15 17:39:00 -0400

    2.2.3: Boolean Variables

--- src/Labs/Lab3/VariableTypes/index.tsx
+++ src/Labs/Lab3/VariableTypes/index.tsx
@@ -1,5 +1,13 @@
 export default function VariableTypes() {
-    
+    /**
+     * Notes:
+     * let: block-scoped variable in javascript - variable limited 
+     *      in scope to block, statement, or expression where used. 
+     *      Note, this is in contrast to "var" keyword which declares
+     *      a variable globally or locally to an entire function
+     * 
+     * {}: In React, expressions enclosed in {} within JSX are JavaScript expressions, and they are evaluated and rendered within the HTML-like syntax. In React, JSX (JavaScript XML) is a syntax extension that allows you to write HTML-like code within JavaScript. JSX makes it easier to create and visualize the structure of your UI components by allowing you to write HTML tags directly in JavaScript.
+     */
     let numberVariable = 123;
     let floatingPointNumber = 234.345;
     let stringVariable = 'Hello World!';
@@ -13,7 +21,10 @@ export default function VariableTypes() {
       numberVariable = { numberVariable }<br/>
       floatingPointNumber = { floatingPointNumber }<br/>
       stringVariable = { stringVariable }<br/>
-      booleanVariable = { booleanVariable + "" }<br/>
+
+      {/* Note that we had to convert the boolean variable into a string type before it could render in the browser. */}
+      booleanVariable = { booleanVariable + "" }<br/> 
+
       isNumber = { isNumber }<br/>
       isString = { isString }<br/>
       isBoolean = { isBoolean }<hr/>
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,3 +1,4 @@
+import BooleanVariables from "./BooleanVariables";
 import VariableTypes from "./VariableTypes";
 import VariablesAndConstants from "./VariablesAndConstants";
 
@@ -5,8 +6,10 @@ export default function Lab3() {
     return(
         <div id="wd-lab3" className="container-fluid">
         <h3>Lab 3</h3>
+        <hr/><hr/>
         <VariablesAndConstants />
         <VariableTypes />
+        <BooleanVariables />
       </div>
     );
 }
