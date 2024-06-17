a84d9095dc6f20b24d171e1072c315e7c2f164a7
sluchak <sluchak.e@northeastern.edu>
2024-06-17 19:14:29 -0400

    2.6.1: Child Components

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -21,6 +21,7 @@ import LegacyFunctions from "./LegacyFunctions"
 import MapFunction from "./MapFunction";
 import SimpleArrays from "./SimpleArrays";
 import Spreading from "./Spreading";
+import Square from "./Square";
 import Styles from "./Styles";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
@@ -64,6 +65,9 @@ export default function Lab3() {
         <Classes />
         <Styles />
         <Add a={3} b={4} />
+        <h4>Square of 4</h4>
+        <Square>4</Square>
+        <hr />
 
     
         
