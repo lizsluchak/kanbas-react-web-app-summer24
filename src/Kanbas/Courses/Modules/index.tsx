39e6b84c2a997f2de18d0a6007812ffe47278f05
sluchak <sluchak.e@northeastern.edu>
2024-06-18 18:42:01 -0400

    FIX ERROR: screen has more padding in home then modules, so resizes between toggling, annoying

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,13 +1,15 @@
-import ModulesControls from "./ModuleControls";
-import CourseStatus from "../Home/Status";
-import ModuleDataTable from "./Module Data Table";
+
 import { Route, Routes, useParams, useLocation} from "react-router"; //took off 
 // import { * } from "../../Database/courses.json";
 import * as db from "../../Database";
+import { BsGripVertical } from "react-icons/bs";
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
 
 
 export default function Modules() {
   const { cid } = useParams();
+  const modules = db.modules; 
   console.log("Course ID:", cid);
   console.log("hello")
 
@@ -15,24 +17,55 @@ export default function Modules() {
   
   const { pathname } = useLocation();
   console.log(pathname);
+
   return (
-    <div id="wd-modules" className="d-flex">
-
-
-      <div className="d-flex flex-column flex-fill">
-  
-        <ModulesControls />
-        <ModuleDataTable />
-      </div>
-
-      <div className="d-none d-lg-block m-3">
-        <CourseStatus />
-      </div>
-
-    </div>
-
-
-
-
-  );
+    <ul id="wd-modules" className="list-group rounded-0">
+      {modules
+        .filter((module: any) => module.course === cid)
+        .map((module: any) => (
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
+            {module.name}
+            <ModuleControlButtons />
+          </div>
+          {module.lessons && (
+            <ul className="wd-lessons list-group rounded-0">
+              {module.lessons.map((lesson: any) => (
+                <li className="wd-lesson list-group-item p-3 ps-1">
+                  <BsGripVertical className="me-2 fs-3" />
+                  {lesson.name}
+                  <LessonControlButtons />
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+);
 }
+
+
+
+
+// return (
+//   <div id="wd-modules" className="d-flex">
+
+
+//     <div className="d-flex flex-column flex-fill">
+
+//       <ModulesControls />
+//       <ModuleDataTable />
+//     </div>
+
+//     <div className="d-none d-lg-block m-3">
+//       <CourseStatus />
+//     </div>
+
+//   </div>
+
+
+
+
+// );
