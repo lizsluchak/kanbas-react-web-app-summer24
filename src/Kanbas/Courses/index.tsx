83e83bdd14a511da58d807d87758b73d3d52dbca
sluchak <sluchak.e@northeastern.edu>
2024-06-17 23:41:28 -0400

    display course name at first click

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -3,23 +3,27 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import { Navigate, Route, Routes } from "react-router";
-import { FaAlignJustify } from "react-icons/fa";
+import { Navigate, Route, Routes, useParams } from "react-router";
+import { FaAlignJustify } from "react-icons/fa6";
 import Quizzes from "./Quizzes";
 import Grades from "./Grades";
+import { courses } from "../Database";
+import { Link } from "react-router-dom";
 
 
 export default function Courses() {
-    return (
-      <div id="wd-courses">
-        {/**ClassName="text-danger" makes header red; then we add icon via
-         * FaAlignJustify + ClassName="padding classes"; some padding for 
-         * the icon*/}
-        <h2 className="text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" /> 
-          Course 1234</h2>
-        <hr />
-
-        <div className="d-flex">
+  const { cid } = useParams();
+  console.log("Course ID:", cid);
+  console.log("hello")
+  const course = courses.find((course) => course._id === cid);
+  return (
+    <div id="wd-courses">
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1"/>
+        {/* <Link to="/src/Kan/Lab3/add/1/2">1 + 2</Link> */}
+        {course && course._id}
+      </h2>
+      <div className="d-flex">
           {/**
            * 4.1 Responsive Design
            * When the screen is too narrow, like when viewed on a phone screen, 
@@ -45,16 +49,60 @@ export default function Courses() {
               </Routes>
             </div>
         </div>
-      </div>
-    );
+     
+    </div>
+  );
 }
 
 
 
 
 
-  //i had to add this export line.. why?
-export {}
+
+
+
+// export default function Courses() {
+//     return (
+//       <div id="wd-courses">
+//         {/**ClassName="text-danger" makes header red; then we add icon via
+//          * FaAlignJustify + ClassName="padding classes"; some padding for 
+//          * the icon*/}
+//         <h2 className="text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" /> 
+//           Course 1234</h2>
+//         <hr />
+
+//         <div className="d-flex">
+//           {/**
+//            * 4.1 Responsive Design
+//            * When the screen is too narrow, like when viewed on a phone screen, 
+//            * we need to hide the KanbasNavigation sidebar. We can use Bootstrap 
+//            * Display Property class d-none to hide the sidebar by default, and 
+//            * then use d-md-block to show the sidebar when the window reaches 
+//            * the medium width screen size. Add Bootstrap Display Property as 
+//            * shown below and confirm the KanbasNavigation sidebar is hidden 
+//            * when the screen is small and then appears as you widen the screen.
+//            */}
+//             <div className="d-none d-md-block">
+//               <CoursesNavigation />
+//             </div>
+      
+//             <div className="flex-fill p-4">
+//               <Routes>
+//                 <Route path="Home" element={<Home />} />
+//                 <Route path="Modules" element={<Modules />} />
+//                 <Route path="Assignments" element={<Assignments />} />
+//                 <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+//                 <Route path="Quizzes" element={<Quizzes />} />
+//                 <Route path="Grades" element={<Grades />} />
+//               </Routes>
+//             </div>
+//         </div>
+//       </div>
+//     );
+// }
+
+
+
 
 /* Iteration Graveyard:
   - Version 1: Dead because uses table elements, and can replace with div 
