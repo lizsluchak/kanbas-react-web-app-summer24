1b67fe4c3839a7950db7517a7e9b48b068d97dc8
sluchak <sluchak.e@northeastern.edu>
2024-06-18 18:34:17 -0400

    Import modules JSON  data

--- src/Kanbas/Database/index.tsx
+++ src/Kanbas/Database/index.tsx
@@ -1,3 +1,4 @@
 /**The Link component is used to create the hyperlinks and encode the course's ID as part of  the path. This can then be used in other components to parse the ID from the path and display content specific to the selected course. Confirm that clicking on different courses encodes the corresponding course ID in the path. Refactor the courses.json and the Dashboard component so that each course has a different image. */
 import courses from "./courses.json";
-export {  courses  };
+import modules from "./modules.json";
+export {  courses, modules  };
