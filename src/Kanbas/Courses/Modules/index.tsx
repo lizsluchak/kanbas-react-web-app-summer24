ed53be1d989d3ea3fafcb526f202ee5e9d686635
sluchak <sluchak.e@northeastern.edu>
2024-06-22 16:06:55 -0400

    module title spacing

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -18,7 +18,7 @@ export default function Modules() {
 
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
 
-          <div className="wd-title p-3 ps-2 bg-secondary">
+          <div className="wd-title p-3 ps-3 bg-secondary">
             <BsGripVertical className="me-2 fs-3" />
             {module.name}
             <ModuleControlButtons />
