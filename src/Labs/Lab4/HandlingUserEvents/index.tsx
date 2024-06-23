bd1842285e71cf5f358b7334ddcb7d6e99b5fa31
sluchak <sluchak.e@northeastern.edu>
2024-06-23 12:53:29 -0400

    edit styling in 2.2

--- src/Labs/Lab4/HandlingUserEvents/index.tsx
+++ src/Labs/Lab4/HandlingUserEvents/index.tsx
@@ -7,16 +7,21 @@ export default function HandlingUserEvents() {
     // implement callback function
     function sayHello() {
         alert("Hello");
-      }
-    
+    }
+
     return (
 
         <div id="handling-user-events">
-            <ClickEvent />
-            <PassingDataOnEvent />
-            <PassingFunctions theFunction={sayHello} /> {/** pass callback function as a parameter */}
-            <EventObject />
-           
+            <br />
+            <h4 className="lab4-intro-definition">2.2: Handling User Events</h4>
+            <ol>
+                <li><ClickEvent /></li>
+                <li><PassingDataOnEvent /></li>
+                <li><PassingFunctions theFunction={sayHello} /> </li> {/** pass callback function as a parameter */}
+                <li><EventObject /></li>
+            </ol>
+
+
 
         </div>
 
---
+++ src/Labs/Lab4/ManagingComponentState/index.tsx
@@ -0,0 +1,17 @@
+
+
+export default function ManagingComponentState() {
+
+
+    
+    return (
+
+        <div id="managing-component-state">
+   
+           
+
+        </div>
+
+
+    );
+}
