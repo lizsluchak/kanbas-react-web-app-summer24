3479e3074dc445560c2077aac0cde3a8846a2aa4
sluchak <sluchak.e@northeastern.edu>
2024-07-06 15:09:09 -0400

    add lab5 to react, add local enviornment variables, render server address

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,25 @@
+
+/**
+ * Lab5 Entry Point: 
+ * 
+ * (1) Test out server creation, link to server redendered Lab5 page via href
+ * @returns
+ */
+
+import EnvironmentVariables from "./EnviornmentVariables";
+
+export default function Lab5() {
+    return (
+      <div id="wd-lab5">
+        <h2>Lab 5</h2>
+        <div className="list-group">
+          <a href="http://localhost:4000/lab5/welcome"          
+             className="list-group-item">
+             Welcome
+          </a>
+          <EnvironmentVariables />
+        </div><hr/>
+      </div>
+    );
+  }
+  
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -21,6 +21,7 @@ import "../index.css"
 import Lab4 from "./Lab4";
 import store from "./store";
 import { Provider } from "react-redux";
+import Lab5 from "./Lab5";
 
 
 export default function Labs() {
@@ -52,6 +53,7 @@ export default function Labs() {
         {/* <Route path="Lab3" element={<Lab3 />} /> */}
         <Route path="Lab3/*" element={<Lab3 />} />
         <Route path="Lab4/*" element={<Lab4 />} />
+        <Route path="Lab5/*" element={<Lab5 />} />
         <Route path="Kanbas/*" element={<Kanbas />} />
        
       </Routes>
