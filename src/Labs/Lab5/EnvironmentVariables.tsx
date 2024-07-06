7523b74fcab946ac5e77963ddecbf9bf652b50c1
sluchak <sluchak.e@northeastern.edu>
2024-07-06 15:46:37 -0400

    3.1 Enviornment Variables working

--- src/Labs/Lab5/EnvironmentVariables.tsx
+++ src/Labs/Lab5/EnvironmentVariables.tsx
@@ -1,20 +1,11 @@
-import EnvironmentVariables from "./EnvironmentVariables";
-
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
-
-
-export default function Lab5() {
-  console.log(REMOTE_SERVER); 
+export default function EnvironmentVariables() {
   return (
-    <div id="wd-lab5">
-      <h2>Lab 5</h2>
-      <div className="list-group">
-        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
-          Welcome
-        </a>
-      </div><hr />
-      <EnvironmentVariables />
+    <div id="wd-environment-variables">
+      <h3>Environment Variables</h3>
+      <p>Remote Server: {REMOTE_SERVER}</p><hr/>
     </div>
   );
 }
+
