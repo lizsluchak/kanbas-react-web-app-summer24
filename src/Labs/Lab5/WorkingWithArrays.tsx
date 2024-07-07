a378665c5ceab224a3513b71d289092de45a0c4f
sluchak <sluchak.e@northeastern.edu>
2024-07-07 09:19:27 -0400

    3.4.1 retrieve array from server

---
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -0,0 +1,16 @@
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+
+/**
+ * Hyperlink to test retrieving server array
+ * @returns 
+ */
+export default function WorkingWithArrays() {
+  const API = `${REMOTE_SERVER}/lab5/todos`;
+  return (
+    <div id="wd-working-with-arrays">
+      <h3>Working with Arrays</h3>
+      <h4>Retrieving Arrays</h4>
+      <a id="wd-retrieve-todos" className="btn btn-primary" href={API}>
+        Get Todos </a><hr/>
+    </div>
+);}
