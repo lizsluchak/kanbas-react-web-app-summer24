506be0f63be3ba5068dc07ceecdce6aa7c37ca6a
sluchak <sluchak.e@northeastern.edu>
2024-07-08 18:46:50 -0400

    3.6.7 error handling

--- src/Labs/Lab5/WorkingWithArraysAsync.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsync.tsx
@@ -6,106 +6,125 @@ import { TiDelete } from "react-icons/ti";
 import { FaPencil } from "react-icons/fa6";
 
 export default function WorkingWithArraysAsynchronously() {
-  const [todos, setTodos] = useState<any[]>([]); //init state variable
-  /**
-   * async function to fetch todos from server and populate todos state
-   * variable which can render on load due to useEffect
-   */
-  const fetchTodos = async () => {
-    const todos = await client.fetchTodos();
-    setTodos(todos);
-  };
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+    const [todos, setTodos] = useState<any[]>([]); //init state variable
+    /**
+     * async function to fetch todos from server and populate todos state
+     * variable which can render on load due to useEffect
+     */
+    const fetchTodos = async () => {
+        const todos = await client.fetchTodos();
+        setTodos(todos);
+    };
+    useEffect(() => {
+        fetchTodos();
+    }, []);
 
-  const removeTodo = async (todo: any) => {
-    const updatedTodos = await client.removeTodo(todo);
-    setTodos(updatedTodos);
-  };
+    const removeTodo = async (todo: any) => {
+        const updatedTodos = await client.removeTodo(todo);
+        setTodos(updatedTodos);
+    };
 
-  //using axios.get
-  const createTodo = async () => {
-    const todos = await client.createTodo();
-    setTodos(todos);
-  };
+    //using axios.get
+    const createTodo = async () => {
+        const todos = await client.createTodo();
+        setTodos(todos);
+    };
 
-  //using axios.post
-  const postTodo = async () => {
-    const newTodo = await client.postTodo({ title: "New Posted Todo", completed: false, });
-    setTodos([...todos, newTodo]);
-  };
+    //using axios.post
+    const postTodo = async () => {
+        const newTodo = await client.postTodo({ title: "New Posted Todo", completed: false, });
+        setTodos([...todos, newTodo]);
+    };
 
-  const editTodo = (todo: any) => {
-    const updatedTodos = todos.map(
-      (t) => t.id === todo.id ? { ...todo, editing: true } : t );
-    setTodos(updatedTodos);
-  };
-  const updateTodo = async (todo: any) => {
-    await client.updateTodo(todo);
-    setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
-  };
+    const editTodo = (todo: any) => {
+        const updatedTodos = todos.map(
+            (t) => t.id === todo.id ? { ...todo, editing: true } : t);
+        setTodos(updatedTodos);
+    };
 
-
-/**
- * Note that the new implementation ignores the response from the server and 
- * instead filters the removed todo from the local state variable. T
- * his is fine for now, but the operation is too optimistic assuming the 
- * server successfully deleted the item from the array and updating the user 
- * interface without confirmation. Later we'll deal with errors from the server 
- * to make sure the local state variable in the user interface is in synch with 
- * the remote array on the server.
-
- * @param todo 
- */
-  const deleteTodo = async (todo: any) => {
-    await client.deleteTodo(todo);
-    const newTodos = todos.filter((t) => t.id !== todo.id);
-    setTodos(newTodos);
-  };
+    //add error handling to catch server error
+    const [errorMessage, setErrorMessage] = useState(null);
+    const updateTodo = async (todo: any) => {
+        try {
+            await client.updateTodo(todo);
+            setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+        } catch (error: any) {
+            setErrorMessage(error.response.data.message);
+        }
 
 
-  return (
-    <div id="wd-asynchronous-arrays">
-      <h3>Working with Arrays Asynchronously</h3>
-      
-      
+    };
 
-      <ul className="list-group w-25">
-      <h4>Todos <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3"
-                         id="wd-create-todo" /> 
-                <FaPlusCircle onClick={postTodo}   className="text-primary float-end fs-3 me-3" id="wd-post-todo"   />
+
+    /**
+     * Note that the new implementation ignores the response from the server and 
+     * instead filters the removed todo from the local state variable. T
+     * his is fine for now, but the operation is too optimistic assuming the 
+     * server successfully deleted the item from the array and updating the user 
+     * interface without confirmation. Later we'll deal with errors from the server 
+     * to make sure the local state variable in the user interface is in synch with 
+     * the remote array on the server.
+    
+     * @param todo 
+     */
+    const deleteTodo = async (todo: any) => {
+        try {
+            await client.deleteTodo(todo);
+            const newTodos = todos.filter((t) => t.id !== todo.id);
+            setTodos(newTodos);
+        } catch (error: any) {
+            console.log(error);
+            setErrorMessage(error.response.data.message);
+
+        }
+    };
+
+
+    return (
+        <div id="wd-asynchronous-arrays">
+            <h3>Working with Arrays Asynchronously</h3>
+            {errorMessage && (
+                <div id="wd-todo-error-message" className="alert alert-danger mb-2 mt-2">{errorMessage}</div>
+            )}
+
+
+
+
+            <ul className="list-group w-25">
+                <h4>Todos <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3"
+                    id="wd-create-todo" />
+                    <FaPlusCircle onClick={postTodo} className="text-primary float-end fs-3 me-3" id="wd-post-todo" />
                 </h4>
-        {todos.map((todo) => (
-          <li key={todo.id} className="list-group-item">
-            <FaPencil onClick={() => editTodo(todo)} className="text-primary float-end me-2 mt-1" />
+                {todos.map((todo) => (
+                    <li key={todo.id} className="list-group-item">
+                        <FaPencil onClick={() => editTodo(todo)} className="text-primary float-end me-2 mt-1" />
 
-            <input type="checkbox" defaultChecked={todo.completed} className="form-check-input me-2 float-start"
-              onChange={(e) => updateTodo({ ...todo, completed: e.target.checked }) } />
-              {!todo.editing ? ( todo.title ) : (
-                <input className="form-control w-50 float-start" defaultValue={todo.title}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      updateTodo({ ...todo, editing: false });
-                    }
-                  }}
-                  onChange={(e) =>
-                    updateTodo({ ...todo, title: e.target.value })
-                  }
-                />
-              )}
+                        <input type="checkbox" defaultChecked={todo.completed} className="form-check-input me-2 float-start"
+                            onChange={(e) => updateTodo({ ...todo, completed: e.target.checked })} />
+                        {!todo.editing ? (todo.title) : (
+                            <input className="form-control w-50 float-start" defaultValue={todo.title}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        updateTodo({ ...todo, editing: false });
+                                    }
+                                }}
+                                onChange={(e) =>
+                                    updateTodo({ ...todo, title: e.target.value })
+                                }
+                            />
+                        )}
 
-            {/* <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+                        {/* <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
               {todo.title}
             </span> */}
-            <FaTrash onClick={() => removeTodo(todo)}
-                     className="text-danger float-end mt-1" id="wd-remove-todo"/>
-            <TiDelete onClick={() => deleteTodo(todo)} className="text-danger float-end me-2 fs-3" id="wd-delete-todo" />
+                        <FaTrash onClick={() => removeTodo(todo)}
+                            className="text-danger float-end mt-1" id="wd-remove-todo" />
+                        <TiDelete onClick={() => deleteTodo(todo)} className="text-danger float-end me-2 fs-3" id="wd-delete-todo" />
 
 
-          </li>
-        ))}
-      </ul> <hr />
-    </div>
-  );
+                    </li>
+                ))}
+            </ul> <hr />
+        </div>
+    );
 }
